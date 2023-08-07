a85c23dfad3943cae5e3f602f2f0b5ef9e4b2999
Katherine Jarboe <110368535+melo616@users.noreply.github.com>
2023-08-07 22:44:35 +0000

    corrected multiply bugs

--- app.rb
+++ app.rb
@@ -28,10 +28,13 @@ get("/wizard_subtract") do
 end
 
 get("/multiply") do
-  erb(:multiplication_form)
+  erb(:mult_form)
 end
 
-get("/wizard multiply") do
+get("/wizard_multiply") do
+  @first_num = params.fetch("first_number").to_f
+  @second_num = params.fetch("second_number").to_f
+  @result = @first_num * @second_num
   erb(:mult_result)
 end
 
